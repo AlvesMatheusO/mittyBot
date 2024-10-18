b478c46b911900dc94c0adc7f151876aa5bec9eb
AlvesMatheusO <matheusalv.oliveira@gmail.com>
2024-10-18 16:57:01 -0300

    list movies on theaters

--- lib/mitty_bot.ex
+++ lib/mitty_bot.ex
@@ -7,49 +7,66 @@ defmodule MittyBot do
   @spec handle_event(any()) ::
           :ignore
           | :noop
-          | {:error,
-             %{
-               response:
-                 binary()
-                 | %{:code => 1..1_114_111, :message => binary(), optional(:errors) => map()},
-               status_code: 1..1_114_111
-             }}
+          | {:error, map()}
           | {:ok, Nostrum.Struct.Message.t()}
+
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
     case msg.content do
       "!hi" ->
         Api.create_message(msg.channel_id, "Hello!")
 
-        #Acessar Diretor
       "!diretor " <> director_name ->
-        case TmdbService.search_director(director_name) do
-          {:ok, movies} ->
-            response = format_movie(movies)
-            Api.create_message(msg.channel_id, response)
+        handle_director_command(msg.channel_id, director_name)
 
-          {:error, _} ->
-            Api.create_message(msg.channel_id, "Não consegui encontrar "<> director_name <> " :sob:")
-        end
+      "!acted " <> actor_name ->
+        handle_actor_command(msg.channel_id, actor_name)
 
-        "!acted " <> actor_name ->
-          case TmdbService.search_actor(actor_name) do
-            {:ok, movies} ->
-              response = format_movie(movies)
-              Api.create_message(msg.channel_id, response)
+      "!cine" ->
+        handle_movies_in_theater(msg.channel_id)
 
-            {:error, _} ->
-               Api.create_message(msg.channel_id, "Não consegui achar " <> actor_name <> " :sob:")
-
-          end
-        # Else:
       _ ->
         :ignore
     end
   end
 
+  defp handle_director_command(channel_id, director_name) do
+    case TmdbService.search_director(director_name) do
+      {:ok, movies} ->
+        response = format_movie(movies)
+        Api.create_message(channel_id, response)
+
+      {:error, _} ->
+        Api.create_message(channel_id, "Não consegui encontrar #{director_name} :sob:")
+    end
+  end
+
+  defp handle_actor_command(channel_id, actor_name) do
+    case TmdbService.search_actor(actor_name) do
+      {:ok, movies} ->
+        response = format_movie(movies)
+        Api.create_message(channel_id, response)
+
+      {:error, _} ->
+        Api.create_message(channel_id, "Não consegui achar #{actor_name} :sob:")
+    end
+  end
+
+  defp handle_movies_in_theater(channel_id) do
+    case TmdbService.movies_on_theaters() do
+      {:ok, movies} ->
+        response = format_movie(movies)
+        Api.create_message(channel_id, response)
+
+      {:error, _} ->
+        Api.create_message(channel_id, "Erro ao buscar filmes em cartaz :sob:")
+    end
+  end
+
+  defp format_movie([]), do: "Nenhum filme encontrado."
+
   defp format_movie(movies) do
     movies
-    |> Enum.map(fn movie -> movie["title"] end)
+    |> Enum.map(fn movie -> "#{movie["title"]} - Lançamento: #{movie["release_date"]}" end)
     |> Enum.join("\n")
   end
 end
--- lib/mitty_bot/services/tmdbService.ex
+++ lib/mitty_bot/services/tmdbService.ex
@@ -1,83 +1,80 @@
 defmodule MittyBot.Services.TmdbService do
   @moduledoc """
-  Serviço para buscar diretores na API do TMDb.
+  Serviço para buscar informações na API do TMDb.
   """
 
   @api_key "f7809065a7efebbcdb9e0cf29f8f2695"
+  @base_url "https://api.themoviedb.org/3"
 
-  # Função para buscar o diretor pelo nome e pegar o ID
   def search_director(director_name) do
-    url =
-      "https://api.themoviedb.org/3/search/person?api_key=#{@api_key}&query=#{URI.encode(director_name)}"
-
-    case HTTPoison.get(url) do
-      {:ok, %{status_code: 200, body: body}} ->
-        case Jason.decode(body) do
-          # Pega o primeiro resultado e seu ID
-          {:ok, %{"results" => [%{"id" => person_id} | _rest]}} ->
-            get_movies(person_id)
-
-          _ ->
-            {:error, :falha_json}
-        end
-
-      _ ->
-        {:error, :api_falhou}
-    end
+    url = "#{@base_url}/search/person?api_key=#{@api_key}&query=#{URI.encode(director_name)}"
+    fetch_person(url, &get_movies/1)
   end
 
   def search_actor(actor_name) do
-    url =
-      "https://api.themoviedb.org/3/search/person?api_key=#{@api_key}&query=#{URI.encode(actor_name)}"
+    url = "#{@base_url}/search/person?api_key=#{@api_key}&query=#{URI.encode(actor_name)}"
+    fetch_person(url, &get_movies_acted/1)
+  end
 
+  defp fetch_person(url, callback) do
     case HTTPoison.get(url) do
       {:ok, %{status_code: 200, body: body}} ->
         case Jason.decode(body) do
-          {:ok, %{"results" => [%{"id" => person_id} | _rest]}} ->
-            get_movies_acted(person_id)
+          {:ok, %{"results" => [%{"id" => person_id} | _]}} ->
+            callback.(person_id)
 
-          _ ->
-            {:error, :api_falhou}
+          _ -> {:error, :falha_json}
         end
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts("Erro ao buscar pessoa: #{inspect(reason)}")
+        {:error, :api_falhou}
     end
   end
 
   defp get_movies(person_id) do
-    url = "https://api.themoviedb.org/3/person/#{person_id}/movie_credits?api_key=#{@api_key}"
-
-    case HTTPoison.get(url) do
-      {:ok, %{status_code: 200, body: body}} ->
-        case Jason.decode(body) do
-          {:ok, %{"crew" => crew}} ->
-            moviesDirected = Enum.filter(crew, fn entry -> entry["job"] == "Director" end)
-            {:ok, moviesDirected}
-
-          _ ->
-            {:error, :falha_json}
-        end
-
-      _ ->
-        {:error, :api_falhou}
-    end
+    url = "#{@base_url}/person/#{person_id}/movie_credits?api_key=#{@api_key}"
+    fetch_credits(url, "Director")
   end
 
   defp get_movies_acted(person_id) do
-    url = "https://api.themoviedb.org/3/person/#{person_id}/movie_credits?api_key=#{@api_key}"
+    url = "#{@base_url}/person/#{person_id}/movie_credits?api_key=#{@api_key}"
+    fetch_credits(url, nil)
+  end
 
+  defp fetch_credits(url, job) do
     case HTTPoison.get(url) do
       {:ok, %{status_code: 200, body: body}} ->
         case Jason.decode(body) do
-          {:ok, %{"cast" => cast}} ->
+          {:ok, %{"crew" => crew}} when job != nil ->
+            movies = Enum.filter(crew, &(&1["job"] == job))
+            {:ok, movies}
 
+          {:ok, %{"cast" => cast}} ->
             {:ok, cast}
 
-          _ ->
-            {:error, :falha_json}
+          _ -> {:error, :falha_json}
         end
 
-      _ ->
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts("Erro ao buscar créditos: #{inspect(reason)}")
         {:error, :api_falhou}
     end
   end
 
+  def movies_on_theaters do
+    url = "#{@base_url}/movie/now_playing?api_key=#{@api_key}&language=pt-BR&page=1&region=BR"
+
+    case HTTPoison.get(url) do
+      {:ok, %{status_code: 200, body: body}} ->
+        case Jason.decode(body) do
+          {:ok, %{"results" => movies}} -> {:ok, movies}
+          _ -> {:error, :falha_json}
+        end
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.puts("Erro ao buscar filmes em cartaz: #{inspect(reason)}")
+        {:error, :api_falhou}
+    end
+  end
 end
